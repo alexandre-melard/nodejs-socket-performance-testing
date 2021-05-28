4e8c665f75bc56b5fc59731c400ecdcb547a6309
Alexandre MELARD <alexandre.melard@richemont.com>
2021-05-28 11:15:22 +0200

    improved performances, logs and added stats

--- client.js
+++ client.js
@@ -1,11 +1,26 @@
 /**
  * call using a distinct arg for each client
  */
-var net = require('net');
+let net = require('net');
+let client = new net.Socket();
+let date;
+let myArgs = process.argv.slice(2);
+let sent = 0;
+let lastSent = 0;
+let received = 0;
+let lastReceived = 0;
+let delay=0;
+const STATS_DELAY = 10000;
 
-var client = new net.Socket();
-var date;
-var myArgs = process.argv.slice(2);
+async function stats() {
+    while (true) {
+        await new Promise(resolve => setTimeout(resolve, STATS_DELAY));
+        console.log(`received: ${lastReceived}, sent ${lastSent} in last ${STATS_DELAY} [s: ${sent}, r: ${received}, avg: ${(delay/lastReceived).toFixed(2)}ms]`);
+        lastReceived = 0;
+        lastSent = 0;
+        delay = 0;
+    }
+}
 
 // Watch the async keyword !
 async function hello() {
@@ -16,22 +31,28 @@ async function hello() {
         // register the date
         date = new Date().getTime()
         // Send the data with the program argument to know who is writing
-        client.write(date + ` Hello, server! Love, Client ${myArgs[0]}.`);
+        client.write(`${myArgs[0]}\n`);
+        sent++;
+        lastSent++;
     }
 }
 
-client.connect(1337, '127.0.0.1', function() {
-	console.log('Connected');
+client.connect(1337, '127.0.0.1', function () {
+    console.log('Connected');
     // need to be async
     hello();
 });
 
-client.on('data', function(data) {
+client.on('data', function (data) {
     // Check time from sent to receive
-	console.log((new Date().getTime() - date) + 'ms Received: ' + data);
+    delay+=new Date().getTime() - date
+    received++;
+    lastReceived++;
 });
 
-client.on('close', function() {
-	console.log('Connection closed');
-	client.destroy(); // kill client after server's response
+client.on('close', function () {
+    console.log('Connection closed');
+    client.destroy(); // kill client after server's response
 });
+
+stats();
--- server.js
+++ server.js
@@ -1,31 +1,111 @@
-var net = require('net');
-var num = 0;
-var sockets = []
 
-// Async !
-async function sendData(s, d) {
-  s.write(d);
+class MessageBuffer {
+  constructor(delimiter) {
+    this.delimiter = delimiter;
+    this.buffer = "";
+  }
+
+  ready() {
+    return !(
+      this.buffer.length === 0 ||
+      this.buffer.indexOf(this.delimiter) === -1
+    );
+  }
+
+  push(data) {
+    this.buffer += data;
+  }
+
+  getMessage() {
+    const delimiterIndex = this.buffer.indexOf(this.delimiter);
+    if (delimiterIndex !== -1) {
+      const message = this.buffer.slice(0, delimiterIndex);
+      this.buffer = this.buffer.replace(message + this.delimiter, "");
+      return message;
+    }
+    return null;
+  }
+
+  handleData() {
+    /**
+     * Try to accumulate the buffer with messages
+     *
+     * If the server isnt sending delimiters for some reason
+     * then nothing will ever come back for these requests
+     */
+    const message = this.getMessage();
+    return message;
+  }
 }
 
-var server = net.createServer(function(socket) {
-  console.log(socket)
-  sockets.push(socket)
-  socket.write(`Echo server ${num}\r\n`);  
-  num += 1;
-  socket.on('error', (e) => {
-    console.log(e);
-  });
-  socket.on('data', d => {
-    /* You might want to keep the foreach 
-       to keep the message flow in line? */
-    sockets.forEach(s => {
-      if (s != socket) {
-        sendData(s, d);
+let net = require('net');
+let num = 0;
+let sockets = new Map();
+const STATS_DELAY = 10000;
+let received = new MessageBuffer("\n")
+
+async function readData(id, data) {
+  received.push(data);
+  if (received.ready()) {
+    const message = received.handleData()
+    let socket = sockets.get(id);
+    let stats = socket.stats;
+    if (!socket.name) {      
+      const name = message.replace(this.delimiter, '');
+      socket.name = name;
+      console.log(`setting name: ${socket.name}, message: ${message}`);
+    }
+    stats.received++;
+    stats.lastReceived++;
+    sockets.forEach(async (s) => {
+      if (s.id != id) {
+        s.socket.write(data);
+        stats.sent++;
+        stats.lastSent++;
       }
     });
-  })
+  }
+}
+
+function avgSpeed(num) {
+  return Math.round(num * 1000 / STATS_DELAY);
+}
+
+async function stats() {
+  while (true) {
+    await new Promise(resolve => setTimeout(resolve, STATS_DELAY));
+    if (sockets.size == 0) {
+      console.log('waiting for client to connect');
+    }
+    sockets.forEach((socket, id) => {
+      let stats = socket.stats;
+      console.log(`client ${socket.name} received ${avgSpeed(stats.lastReceived)} msg/s and sent ${avgSpeed(stats.lastSent)} msg/s [s: ${stats.sent}, r: ${stats.received}]`);
+      stats.lastReceived = 0;
+      stats.lastSent = 0;
+    });
+  }
+}
+
+let server = net.createServer(function (socket) {
+  let id = num;
+  console.log(socket)
+  sockets.set(id, {
+    'name': undefined,
+    'socket': socket,
+    'stats': {
+      'received': 0,
+      'sent': 0,
+      'lastReceived': 0,
+      'lastSent': 0
+    }
+  });
+  socket.write(`Echo server ${num}\r\n`);
+  num += 1;
+  socket.on('error', (e) => console.log(e));
+  socket.on('data', (d) => readData(id, d));
 });
 
 
 
 server.listen(1337, '127.0.0.1');
+stats();
